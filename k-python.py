98f2518b8247b30a6111d67a67235bab92f95241
Ankit Thakur <ankit.thakur@infoorigin.com>
2025-07-18 15:01:06 +0530

    kubernetes imp topics

---
+++ k-python.py
@@ -0,0 +1,16 @@
+from kubernetes import client, config
+import base64
+config.load_kube_config()
+#initializing kuberntes client to fetch resources
+v1 = client.CoreV1Api()
+print("The secrets values listed below are: ")
+print("----------------------------------------------------")
+secret_name="dev-info-recommendation"
+namespace="dev-convo-sphere"
+raw_secret = (v1.read_namespaced_secret(secret_name, namespace)).data
+for key, value in raw_secret.items():
+#   secret = (str(base64.b64decode(value).strip())).replace('b', '', 1) 
+  secret = (base64.b64decode(value).decode('utf-8')).strip()
+  print(f"{key}: {secret}")
+
+
